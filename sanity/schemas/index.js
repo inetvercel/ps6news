f4bdd11888b62ef1c21f53aae65bd330d4dc61c7
inetvercel <gamerhomenetwork@gmail.com>
2026-05-31 11:13:06 +0100

    fix: delete stale duplicate .js schema files that shadowed .ts and hid htmlEmbed/youtube blocks

--- sanity/schemas/index.js
+++
@@ -1,9 +0,0 @@
-import article from './article'
-import page from './page'
-import category from './category'
-import author from './author'
-import poll from './poll'
-import keyTakeaways from './keyTakeaways'
-import {table, tableRow} from './table'
-
-export const schemaTypes = [article, page, category, author, poll, keyTakeaways, table, tableRow]
